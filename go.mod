6da37610fc9580e1f7a9ccfd75e6cd026c242654
moyrn <Chengpin430124>
2022-02-28 23:30:25 +0800

    init

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/moyrne/vconfig
 go 1.16
 
 require (
-	github.com/go-git/go-git-fixtures/v4 v4.3.1 // indirect
-	github.com/go-git/go-git/v5 v5.4.2 // indirect
+	github.com/go-git/go-git/v5 v5.4.2
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.7.0
 )
--- main.go
+++ main.go
@@ -1,22 +1,122 @@
 package vconfig
 
 import (
-	fixtures "github.com/go-git/go-git-fixtures/v4"
+	"regexp"
+
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/config"
-	"github.com/go-git/go-git/v5/plumbing/cache"
-	"github.com/go-git/go-git/v5/storage/filesystem"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	"github.com/pkg/errors"
 )
 
 type VConfig struct {
-	*git.Remote
+	remote      string
+	user        string
+	pass        string
+	privateKeys []byte
+	publicKeys  *ssh.PublicKeys
+	tag         string
+	cloneDir    string
+
+	repo     *git.Repository
+	workTree *git.Worktree
 }
 
-func New(user, host, pass string) *VConfig {
-	store := filesystem.NewStorage(fixtures.ByURL("").ByTag("").One().DotGit(), cache.NewObjectLRUDefault())
-	return &VConfig{Remote: git.NewRemote(store, &config.RemoteConfig{})}
+var (
+	gitRegex  = regexp.MustCompile(`[a-zA-Z0-9]+@([a-zA-Z0-9\.-]+):([a-zA-Z0-9/-]+).git`)
+	httpRegex = regexp.MustCompile(`(https|http)://[\w+.-]+/([\w+.-]+)/([\w+.-]+)`)
+)
+
+func New(remote, tag, user, pass string, privateKeys []byte) (config *VConfig, err error) {
+
+	v := &VConfig{
+		remote:      remote,
+		user:        user,
+		pass:        pass,
+		privateKeys: privateKeys,
+		tag:         tag,
+		cloneDir:    remote + "-" + tag,
+	}
+
+	if len(privateKeys) != 0 {
+		v.publicKeys, err = ssh.NewPublicKeys(user, privateKeys, pass)
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+	}
+	return v, nil
 }
 
-func (v VConfig) Init() {
+func (v *VConfig) clone() (err error) {
+	v.repo, err = git.PlainOpen(v.cloneDir)
+	if err == git.ErrRepositoryNotExists {
+		options := &git.CloneOptions{URL: v.remote}
+		if v.publicKeys != nil {
+			options.Auth = v.publicKeys
+		}
+		v.repo, err = git.PlainClone(v.cloneDir, false, options)
+	}
+	return errors.WithStack(err)
+}
 
+func (v *VConfig) fetch() (err error) {
+	options := &git.FetchOptions{RemoteName: git.DefaultRemoteName}
+	if v.publicKeys != nil {
+		options.Auth = v.publicKeys
+	}
+	err = v.repo.Fetch(options)
+	if err == nil || err == git.NoErrAlreadyUpToDate {
+		return nil
+	}
+
+	return errors.WithStack(err)
+}
+
+func (v *VConfig) checkout(hash *plumbing.Reference) (err error) {
+	head, err := v.repo.Head()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	if head.Hash().String() == hash.Hash().String() {
+		return nil
+	}
+
+	return errors.WithStack(v.workTree.Checkout(&git.CheckoutOptions{
+		Hash: hash.Hash(),
+	}))
+}
+
+func (v *VConfig) pull() (err error) {
+	options := &git.PullOptions{RemoteName: git.DefaultRemoteName}
+	if v.publicKeys != nil {
+		options.Auth = v.publicKeys
+	}
+	return errors.WithStack(v.workTree.Pull(options))
+}
+
+// Init "https://github.com/moyrne/go-zero"
+func (v *VConfig) Init() error {
+	if err := v.clone(); err != nil {
+		return err
+	}
+	if err := v.fetch(); err != nil {
+		return err
+	}
+
+	ref, err := v.repo.Tag(v.tag)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	v.workTree, err = v.repo.Worktree()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	if err := v.checkout(ref); err != nil {
+		return err
+	}
+
+	return v.pull()
 }
